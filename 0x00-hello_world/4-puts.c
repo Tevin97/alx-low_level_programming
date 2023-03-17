389836de0158a5836b9833b293aace7a541efc4a
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-03-17 15:21:32 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with put function
+ * return: 0 (success!) zero when the program is successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
