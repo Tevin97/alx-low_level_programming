0d49e1875fa790215517d572df53418318e7a6fb
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-03-17 18:49:00 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - program prints the alphabet in lowercase,
+ * followed by alphabets in capital.
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char alphalower[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; alphalower[i] != '\0';
+			putchar(alphalower[i]);
+	}
+	putchar('\n');
+	return (0);
+}
