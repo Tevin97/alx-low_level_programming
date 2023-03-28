b1c5b04a205badfc22a00140d9cbd79c7818af90
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-03-27 17:31:41 -0700

    program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,44 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_PASSWORD_LENGTH 10
-
-char *generate_password(void);
-
 /**
- * main - Entry point
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    generate_password();
-    return (0);
-}
+	int pass[100];
+	int i, sum, n;
 
-/**
- * generate_password - Generates a random password for crackme 101
- *
- * Description: A valid password for crackme 101 must contain only 62 different
- * characters.
- *
- * Return: The generated random password
- */
-char *generate_password(void)
-{
-    char *password, *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    size_t i;
+	sum = 0;	
 
-    srand(time(NULL));
-    password = malloc(MAX_PASSWORD_LENGTH + 1); /* Add 1 for null terminator */
+	srand(time(NULL));
 
-    if (!password)
-        return (NULL);
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-    for (i = 0; i < MAX_PASSWORD_LENGTH; i++)
-        password[i] = charset[rand() % 62];
-
-    password[MAX_PASSWORD_LENGTH] = '\0';
-    printf("%s", password);
-    return (password);
+	return (0);
 }
