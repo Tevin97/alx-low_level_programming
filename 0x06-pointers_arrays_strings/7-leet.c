e0d3ab4b9d69783b9a897e20d7f317cabcef9ede
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-03-30 19:31:11 -0700

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @str: input value
+ * Return: str value
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+			}
+		}
+	}
+	return (str);
+}
