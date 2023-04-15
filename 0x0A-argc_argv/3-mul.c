e0323fbe38af72539401a3c10cf88272bd5bfe7b
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-04-15 06:41:22 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0, num2 = 0, prod;
+	int num1 = 0, num2 = 0;
 
-	if (argc > 3 || argc < 3)
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
 	{
 		printf("error\n");
 		return (1);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-	printf("%d\n", prod);
 	return (0);
 }
