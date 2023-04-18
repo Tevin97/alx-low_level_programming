267b912c9cbb3445685d6b4138a03eb05aebe602
Tevin Murimi <tevinurimimwenda@gmail.com>
2023-04-17 18:32:13 -0700

    Define a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * my_dog - typedef for struct dog
+ */
+
+typedef struct dog my_dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
