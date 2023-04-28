a5055c0e2da7741f801c045cf28da6087aaa059d
Tevin97 <tevinmurimimwenda@gmail.com>
2023-04-28 00:29:30 -0700

    64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,20 @@
-section .data    
-    hello db 'Hello, Holberton',0
-    format db '%s\n',0
+section .data
+	hello db 'Hello, Holberton',0
+	format db '%s\n',0
 
 section .text
-    global main
+	global main
 
 main:
-    push rbp        ; set up stack frame 
-    mov rbp,rsp
-     
-    sub rsp,16      ; allocate space for arguments
-    lea rdi,[hello] ; first argument: pointer to hello string
-    mov rsi,format  ; second argument: pointer to format string
-    xor eax,eax     ; clear EAX register for printf return value
-    call printf     ; call printf function
-    
-    add rsp, 16     ; deallocate arguments
-    mov rsp,rbp     ; restore stack pointer
-    pop rbp         ; restore base pointer
-    xor eax,eax     ; return 0 at end of program
-    ret
+	push rbp
+	mov rbp,rsp
+	sub rsp,16
+	lea rdi,[hello]
+	mov rsi,format
+	xor eax,eax
+	call printf
+	add rsp,16
+	mov rsp,rbp
+	pop rbp
+	xor eax,eax
+	ret
