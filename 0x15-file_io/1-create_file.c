3d80f45ddb54b20d6ce942ffe0e6e07b616a6d14
Tevin97 <tevinmurimimwenda@gmail.com>
2023-05-08 13:49:26 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,27 +8,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wcount, tcount;
+	int fd, wcount, tcount = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		while (text_content[tcount] != '\0')
+			tcount++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		tcount = 0;
-		while (text_content[tcount] != '\0')
-			tcount++;
+	wcount = write(fd, text_content, tcount);
 
-		wcount = write(fd, text_content, tcount);
-		if (wcount == -1 || wcount != tcount)
-		{
-			close(fd);
-			return (-1);
-		}
+	if (wcount == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
