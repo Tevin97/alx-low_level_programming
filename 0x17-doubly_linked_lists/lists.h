cbad88e4062686d2cba2783062b3cd4a024f97ec
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-15 13:13:29 -0700

    add header file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
