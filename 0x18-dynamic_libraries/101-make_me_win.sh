e879c81c0b3cf2d13d25a3bd12bc2d80dca46d8e
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-23 19:23:19 -0700

    add shell script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/lib-101-make_me_win.so https://github.com/Tevin97/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib-101-make_me_win.so
-export LD_PRELOAD=/tmp/lib-101-make_me_win.so
+wget -P /tmp https://github.com/Tevin97/alx-low_level_programming/blob/a52a795399c89df4ebbb62302bf12fc644c48464/0x18-dynamic_libraries/lib-101-test.so
+export LD_PRELOAD=/tmp/lib-101-test.so
