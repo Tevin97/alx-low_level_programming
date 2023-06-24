15d0d9ccd18ae274a958a02ad610d390fa55e196
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-23 19:17:04 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib-101-make_me_win.so https://github.com/Tevin97/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib-101-make_me_win.so
-export LD_PRELOAD=$HOME/lib-101-make_me_win.so 
+wget -P /tmp/lib-101-make_me_win.so https://github.com/Tevin97/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib-101-make_me_win.so
+export LD_PRELOAD=/tmp/lib-101-make_me_win.so
